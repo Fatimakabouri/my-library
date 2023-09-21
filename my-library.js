49529008cd465dc7ce03ced0caca187d1d74a9f0
fkabouri <fatimakabouri@outlook.com>
2023-09-21 11:50:33 +0100

    test iframe

--- my-library.js
+++ my-library.js
@@ -1,7 +1,36 @@
 // my-library.js
 
 const myLibrary = {
-    sayHello : function(){
-        console.log("Hello from my library");
-    },
-}
+  sayHello: function () {
+    console.log("Hello from my library");
+  },
+
+  mountIframe: function (containerId, userData) {
+    const container = document.getElementById(containerId);
+
+    if (!container) {
+      console.error("container not found");
+      return;
+    }
+
+    //create an iframe element
+    const iframe = document.createElement("iframe");
+
+    //define the content of the iframe
+    const iframeContent = `
+        <html>
+            <head>
+                <title>My Iframe</title>
+            </head>
+            <body>
+                <h1>Hello ${userData.name}</h1>
+            </body>
+            </html>
+        `;
+
+    // Set the iframe's content
+    iframe.srcdoc = iframeContent;
+    // Append the iframe to the container
+    container.appendChild(iframe);
+  },
+};
